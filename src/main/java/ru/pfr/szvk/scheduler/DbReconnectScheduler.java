9ceb60ca9bc0b5c9e733819c5bfa8615ac23d87d
092goncharvv@0092.PFR.RU <vladwg@yandex.ru>
2023-02-22 10:32:03 +0300

    Added is crone connect to db.

--- src/main/java/ru/pfr/szvk/scheduler/DbReconnectScheduler.java
+++ src/main/java/ru/pfr/szvk/scheduler/DbReconnectScheduler.java
@@ -9,23 +9,32 @@ import ru.pfr.szvk.WraperM;
 
 
 import java.io.File;
+import java.sql.SQLException;
 
 @Component
 public class DbReconnectScheduler {
-    @Scheduled(cron = "0 0 5 * * MON",zone = "Europe/Moscow")
+    @Scheduled(cron = "0 0 5 * * *",zone = "Europe/Moscow")
 //    @Scheduled(fixedRate = 100)
     public void trackOverduePayments() {
         PropertyConfigurator.configure(String.join("",new File("").getAbsolutePath(),String.join(File.separator,File.separator,"src","main","resources","log4j.properties")));
 
-        this.dbHandler = this.wraperM.getModel().getConnectDb();
+        try {
+            this.dbHandler = this.wraperM.getModel().getConnectDb();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
 
         log.info("Начат процесс переподключение к  Базе");
-        if (this.dbHandler.getConnection() == null) {
             this.dbHandler.setConnection();
             log.info("Переподключение к  Базе выполнено, т.к. ранее было разорвано соединение с Базой.");
-        }else{
-            log.info("Переподключение к  Базе не потребовалось т.к. соединение с базой установлено ранее и не разрывалось");
-        }
+
+//            log.info(String.join(" ","Переподключение к  Базе не потребовалось т.к. соединение с базой установлено ранее и не разрывалось ",this.dbHandler.getConnection().toString()) );
+            if (this.dbHandler.getConnection().equals(null)){
+                log.info("Is not connection");
+            }else {
+                log.info("It is connection");
+            }
+
 
         log.info("Процесс переподключения к БД завершон");
 
